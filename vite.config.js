a7703e201d86580037dac68a7aff7c623c168267
Gentian Polovina <gentip@gmail.com>
2022-01-23 05:58:21 +0000

    add posthtml configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import WindiCSS from 'vite-plugin-windicss'
+import { posthtmlPlugin } from 'vite-plugin-posthtml'
+import include from 'posthtml-include'
 
 export default {
   plugins: [
-    WindiCSS()
+    WindiCSS(),
+    posthtmlPlugin({
+      plugins: [
+        include()
+      ]
+    })
   ]
 }
